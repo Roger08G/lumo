e9bc635199de03d0ac0703e945596d1b48578627
Roger08G <202710647+Roger08G@users.noreply.github.com>
2026-08-14 16:48:14 +0200

    chore(security): tighten mobile release surface

--- mobile/src-tauri/src/lib.rs
+++ mobile/src-tauri/src/lib.rs
@@ -12,7 +12,6 @@ use state::BackendState;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
         .plugin(mobile::init())
         .setup(|app| {
             #[cfg(mobile)]
